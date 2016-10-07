afeeeefa4a8108d9bb307e5c3b13c41db22eac25
James <James@JamesDesktop>
2016-10-06 23:46:55 -0700

    see last

--- jsonWrite.py
+++ jsonWrite.py
@@ -5,30 +5,6 @@ Created on Oct 6, 2016
 """
 import json
 
-# dataString = {
-#     "user": "testUser",
-#     "password": "password1",
-#     "Garden": {
-#         "Name" : "defaultGarden",
-#         "hardware" : {
-#             "tempNames" : ["temp0", "temp1"],
-#             "tempChan"  : [0,1],
-#             "moistName" : ["moist0", "moist1"],
-#             "moistChan" : [19, 18]
-#              }
-#         }
-# }
-# 
-# # Writing JSON data
-# with open("data.json", "w") as f:
-#     json.dump(dataString, f)
-#     
-# # Reading data back
-# with open("data.json", "r") as f:
-#      data = json.load(f)
-#      
-#      print(data)
-
 data = {}
 hardware = {}
 tempNames= {}
