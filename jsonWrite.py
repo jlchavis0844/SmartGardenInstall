5c02fd3cc3db6ae6e1153fc58352458bfd598a17
James <James@JamesDesktop>
2016-10-06 23:46:40 -0700

    jsonData is now reading and writing and buidling. Need to add functions from other setup files for list generation.

--- jsonWrite.py
+++ jsonWrite.py
@@ -5,26 +5,54 @@ Created on Oct 6, 2016
 """
 import json
 
-dataString = {
-    "user": "testUser",
-    "password": "password1",
-    "Garden": {
-        "Name" : "defaultGarden",
-        "hardware" : {
-            "tempNames" : ["temp0", "temp1"],
-            "tempChan"  : [0,1],
-            "moistName" : ["moist0", "moist1"],
-            "moistChan" : [19, 18]
-             }
-        }
-}
+# dataString = {
+#     "user": "testUser",
+#     "password": "password1",
+#     "Garden": {
+#         "Name" : "defaultGarden",
+#         "hardware" : {
+#             "tempNames" : ["temp0", "temp1"],
+#             "tempChan"  : [0,1],
+#             "moistName" : ["moist0", "moist1"],
+#             "moistChan" : [19, 18]
+#              }
+#         }
+# }
+# 
+# # Writing JSON data
+# with open("data.json", "w") as f:
+#     json.dump(dataString, f)
+#     
+# # Reading data back
+# with open("data.json", "r") as f:
+#      data = json.load(f)
+#      
+#      print(data)
 
-# Writing JSON data
-with open('data.json', 'w') as f:
-    json.dump(dataString, f)
-    
-# Reading data back
-with open('data.json', 'r') as f:
-     data = json.load(f)
-     
-     print(data)
+data = {}
+hardware = {}
+tempNames= {}
+tempChan= {}
+moistNames= {}
+moistChan={}
+name=garden ={}
+tempNames["tempNames"] = ["temp0", "temp1"]
+tempChan["tempChan"] = [0,1]
+moistNames["moistNames"] = ["moist0", "moist1"]
+moistChan["moistChan"] = [19,18]
+
+garden["hardware"] = [tempNames, tempChan,moistNames,tempChan]
+
+garden["Name"] = "defaultGard"
+print(garden)
+data["user"] = "testUser"
+data["password"] = "password1"
+data["Garden"] = garden
+print(data)
+with open("data.json", "w") as f:
+    json.dump(data, f)
+
+with open("data.json", "r") as f:
+    data = json.load(f)
+
+print(data)
